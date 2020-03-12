a2a524bd493572d14ada9e7d918f786149756717
Alison Law <alisparklaw@gmail.com>
2020-03-12 12:48:04 +0000

    index.js basic config express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hi there")
+require("dotenv").config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+const PORT = process.env.PORT || 8000
+ 
+ 
+app.get('/api', (req, res) => {
+ res.json({ message: 'The api is UP' })
+})
+app.listen(PORT, () => {
+ console.log(`Listening on port ${PORT}`);
+})
