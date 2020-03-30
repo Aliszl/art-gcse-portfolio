404e05f3dc90dfcdbc5ff007af829b3d2aee8768
Alison Law <alisparklaw@gmail.com>
2020-03-30 14:14:37 +0100

    getById and getByArtist endpoints functional

--- artwork/artwork-helpers.js
+++ artwork/artwork-helpers.js
@@ -1,10 +1,48 @@
 const db = require('../dbConfig');
 
 module.exports = {
-
-    find   
+    getArt,
+    getArtByArtistId, 
+    getArtById,
+    getArtByTheme 
   };
 
-  function find() {
+  function getArt() {
     return db('artwork').select('id', 'title','description','source_image', 'theme_id', 'artist_id');
   }
+
+  function getArtByArtistId (artist_id) {
+      // raw sql SELECT id, title, description,
+      // theme_id, artist_id, 
+      //source_image FROM `artwork`WHERE artist_id=1;
+    return db('artwork')
+    .where({artist_id})
+    .select("id", "title", "description", "theme_id", "artist_id", "source_image");
+  }
+
+  function getArtById(id) {
+    // raw sql SELECT id, title, description,
+    // theme_id, artist_id,
+    // source_image FROM `artwork` WHERE id=1;
+  return db('artwork')
+  .where("id", id)
+  .select("id", "title", "description", "theme_id", "artist_id", "source_image");
+}
+
+function getArtByTheme(theme_id) {
+    // raw sql SELECT id, title, description,
+    // theme_id, artist_id,
+    // source_image FROM `artwork`WHERE theme_id=1;
+  return db('artwork')
+  .where("theme_id", theme_id)
+  .select("id", "title", "description", "theme_id", "artist_id", "source_image");
+}
+
+function getThemeByArtist(artist_id, theme_id) {
+    // raw sql SELECT id, title, description,
+    // theme_id, artist_id,
+    // source_image FROM `artwork`WHERE theme_id=1;
+  return db('artwork')
+  .where("theme_id", theme_id)
+  .select("id", "title", "description", "theme_id", "artist_id", "source_image");
+}
--- artwork/artwork-router.js
+++ artwork/artwork-router.js
@@ -3,13 +3,48 @@ const helpers = require("./artwork-helpers");
 // const restricted = require("../auth/restricted-middleware.js");
 
 //   /api/artwork
-
+ //Get all art
 router.get("/", (req, res) => {
-    helpers.find()
+    helpers.getArt()
       .then(art => {
         res.json(art);
       })
       .catch(err => res.send(err));
   });
+  router.get("/artist/:artist_id", async (req, res) => {
+      try {
+        const { artist_id }= req.params;
+     
+      const artPiece = await helpers.getArtByArtistId(artist_id);
+      res.json(artPiece);
+    } catch (err) {
+      res.status(500).json({
+        message: "Failed to get artwork for this artist"
+      });
+    }
+  });
+  router.get("/artist/:artist_id/:theme/:id", async (req, res) => {
+    try {
+    //   const { artistId }= req.params;
+      const artPiece = await helpers.getArtByTheme('artist_id');
+      res.json(artPiece);
+    } catch (err) {
+      res.status(500).json({
+        message: "Failed to get artwork for this artist"
+      });
+    }
+  });
+  router.get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const artPiece = await helpers.getArtById(id);
+      res.json(artPiece);
+    } catch (err) {
+      res.status(500).json({
+        message: "Failed to get artwork for  id"
+      });
+    }
+  });
+ 
   
   module.exports = router;
